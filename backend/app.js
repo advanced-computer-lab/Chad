c2d24828856ae11a714d30bb01e3edd29f437bf0
mahmednabil109 <mahmednabil109@gmail.com>
2021-11-01 00:08:41 +0200

    init: initiate all the needed folder to start
    - add folder structure for both back/frontend
    - add linting and formatting scripts for backend
    - add needed packages for both back/frontend

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+// require the libraries
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+// require controllers & routers
+
+// GLOCAL OBJECTS
+
+// init the env variables
+dotenv.config();
+
+// init the database
+mongoose
+  .connect(process.env.DB_URI)
+  .then(() => {
+    console.log('[LOG] DB CONNECTED SUCCESSFULY');
+  })
+  .catch((err) => {
+    console.log('[ERR] ERR WHILE CONNECTED TO THE DB\n', err);
+  });
+
+// init the app
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// init the middlewares
+app.use(express.json());
+app.use(cors());
+
+// add routes&controllers
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`[LOG] app is up and running at http://localhost:${PORT}`);
+});
