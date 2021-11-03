36ed99e60b36f75de606f749c3992dbf21ba7eeb
Samer-Walaa <samer.samora111@gmail.com>
2021-11-03 23:32:39 +0200

    flight view with pagenation

--- backend/Routes/flightController.js
+++ backend/Routes/flightController.js
@@ -3,88 +3,35 @@ const router = express.Router();
 const Flight = require('../Models/Flight');
 
 //git req to view all the flights
-router.get('/', (req, res) => {
+router.get('/all/:page', (req, res) => {
+  const page = req.params.page;
   Flight.find()
+    .skip((page - 1) * 20)
+    .limit(20)
     .then((data) => {
-      res.status(200).send(data);
+      res.status(200).send({ success: true, flights: data });
     })
     .catch((err) => {
-      console.log(err);
+      res.status(500).send({ success: false, message: err.message });
     });
 });
 
-// post req to add a flight
-router.post('/', (req, res) => {
-  Flight.find({ flightNumber: req.body.flightNumber })
+//git req to view a required flight
+router.get('/:flightNumber', (req, res) => {
+  Flight.find({ flightNumber: parseInt(req.params.flightNumber) })
     .then((data) => {
       if (data.length == 0) {
-        let flight = Flight(req.body);
-        // flight.creatorId = req.session._id;
-        // if(req.session.role == "admin"){
-        Flight.collection.insertOne(flight);
-        res.status(201).send(flight);
-        // }
-        // else{
-        //     res.status(401).send("You are not authorizer to do such action");
-        // }
+        res.status(400).send({
+          success: false,
+          message: 'There is no fligths with this number',
+        });
       } else {
-        res
-          .status(400)
-          .send('There is a flight with this number already created');
+        res.status(200).send({ success: true, flight: data });
       }
     })
     .catch((err) => {
-      console.log(err);
+      res.status(500).send({ success: false, message: err.message });
     });
-});
-
-//patch req to update a flight using an id
-router.patch('/:flightNumber', (req, res) => {
-  Flight.find({ flightNumber: parseInt(req.params.flightNumber) }).then(
-    (data) => {
-      if (data.length == 1) {
-        // if(req.session.role == "admin" && req.session._id == (data[0]).creatorId){
-        Flight.collection
-          .updateOne(
-            { flightNumber: parseInt(req.params.flightNumber) },
-            { $set: req.body }
-          )
-          .then((data) => {
-            res.status(200).send(data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-        // }
-        // else{
-        //     res.status(401).send("You are not authorizer to do such action");
-        // }
-      } else {
-        res.status(400).send('There is no flight with this number');
-      }
-    }
-  );
-});
-
-//delete req
-router.delete('/:flightNumber', (req, res) => {
-  Flight.find({ flightNumber: parseInt(req.params.flightNumber) }).then(
-    (data) => {
-      // if(req.session.role == "admin" && req.session._id == (data[0]).creatorId){
-      Flight.collection
-        .deleteOne({ flightNumber: parseInt(req.params.flightNumber) })
-        .then((data) => {
-          res.status(200).send(data);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      // }
-      // else{
-      //     res.status(401).send("You are not authorizer to do such action");
-      // }
-    }
-  );
 });
 
 module.exports = router;
