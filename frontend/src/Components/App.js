b1c8ac1ecc7e5066b4f4eacf28e2dd11785931ec
mahmednabil109 <mahmednabil109@gmail.com>
2021-11-04 19:17:17 +0200

    feat: add flight search form UI

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,5 +1,27 @@
+import { Switch, Route } from "react-router-dom";
+import UserContext from "../Context/UserContext";
+import AppBar from "./AppBar";
+import Home from "../Pages/Home";
+import { useState } from "react";
+
 function App() {
-  return <h1>HI from app</h1>;
+  const [userData, setUserData] = useState({});
+
+  return (
+    <UserContext.Provider
+      value={{
+        userData,
+        setUserData,
+      }}
+    >
+      <AppBar />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+      </Switch>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
