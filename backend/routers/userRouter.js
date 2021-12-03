ae86aa5561eea40c32952db81f74ffe8ffdbbd76
Samer-Walaa <samer.samora111@gmail.com>
2021-12-03 12:28:54 +0200

    put request added

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,9 +3,36 @@ const User = require('../models/UserModel');
 
 const userRouter = new Router();
 
+// remove the fields that cannot be modified
+const sanatizeData = (data) => {
+  ['role', '_id'].forEach((f) => delete data[f]);
+  return data;
+};
+
 userRouter.get('/user-info', async (req, res) => {
   try {
     let user = await User.getUserInfo(req.userData);
+    res.status(200).json({
+      success: true,
+      msg: 'ok',
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      msg: 'some db err',
+      err,
+    });
+  }
+});
+
+userRouter.put('/user/:userId', async (req, res) => {
+  try {
+    const _id = req.params.userId;
+    // remove unmodified data
+    const newData = sanatizeData(req.body);
+    const user = await User.updateOne({ _id }, { $set: newData });
+
     res.status(200).json({
       success: true,
       msg: 'ok',
