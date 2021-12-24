ca52f26a050aed0b00f666e6bfae557c4e3caefb
Marwanx10 <marwan.mohsen1000@gmail.com>
2021-12-24 04:02:05 +0200

    make payment / refund done

--- backend/routers/ticketRouter.js
+++ backend/routers/ticketRouter.js
@@ -6,7 +6,9 @@ const sendMail = require('../controllers/mailSender');
 const User = require('../models/UserModel');
 const Flight = require('../models/flightModel');
 const { ADMIN } = require('../constants/userEnum');
-
+const stripe = require('stripe')(
+  'sk_test_51K7yQbHzMRw1OlaDIJ5ncNimeLrpygHJZ5adZZ23LxLaCTyxJ8nQQS0iGrSbc9ipwWxDC5ibJOnQK3UCzeK2LPZv004K4FN79G'
+);
 //TODO : reimplement sanatizeData
 const sanatizeData = (data) => {
   ['creatorId', '_id'].forEach((f) => delete data[f]);
@@ -88,6 +90,11 @@ router.put('/ticket/:ticketId', async (req, res) => {
 
 router.delete('/ticket/:ticketId', async (req, res) => {
   try {
+    //TODO get the charge PID from ticket / reservation model whatever you want
+    const paymentId = 'ch_3KA2g2HzMRw1OlaD1unDA6OQ';
+    const refund = await stripe.refunds.create({
+      charge: paymentId,
+    });
     let permission = false;
     let deletedTickets = [];
     let { email } = await User.findOne({ _id: req.userData.id });
@@ -146,7 +153,12 @@ router.delete('/ticket/:ticketId', async (req, res) => {
         } else {
           await Reservation.findOneAndDelete(reservation);
         }
-        await sendMail(email, 'Cancel ticket', `You canceled your ticket`);
+        await sendMail(
+          email,
+          'Cancel ticket',
+          `You canceled your ticket`,
+          `amount refunded: ${refund.amount}`
+        );
       } else {
         for (let ticketId of reservation.tickets) {
           let _ticket = await Ticket.findOneAndDelete({ _id: ticketId });
